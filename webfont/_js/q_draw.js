7859f796d112a39fe04f743c367bdf371eef1064
June <jyoon10@risd.edu>
2018-12-07 00:40:05 -0500

    webfont upload again

---
+++ webfont/_js/q_draw.js
@@ -0,0 +1,27 @@
+// 20=light green
+// 75=purple
+// 90=pink
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 100);
+  ellipseMode(CENTER);
+  angleMode(DEGREES);
+  // noCursor();
+}
+
+function draw() {
+  background(75, 100, 100);
+  noStroke();
+
+  fill(20, 100, 100);
+  ellipse(windowWidth/2, windowHeight/2, mouseX, mouseY);
+
+  fill(90, 100, 100);
+  triangle(windowWidth/2, windowHeight/2, windowWidth/2, windowHeight, windowWidth, windowHeight);
+
+  // translate(windowWidth/2, windowHeight/2);
+  // rotate(30);
+  // rect(0, 0, mouseX*1.5, windowWidth);
+
+}
---
+++ webfont/_js/u_draw.js
@@ -0,0 +1,18 @@
+// 20=light green
+// 75=purple
+// 90=pink
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 100);
+  ellipseMode(CENTER);
+  // noCursor();
+}
+
+function draw() {
+  background(75, 100, 100);
+  noStroke();
+
+  fill(20, 100, 100);
+  ellipse(windowWidth/2, 0, windowWidth, mouseY*2);
+}
