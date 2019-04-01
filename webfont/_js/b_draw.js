ea031559bab1d5ad7b3bf595963678e34277b2a2
June <jyoon10@risd.edu>
2019-03-31 20:15:34 -0400

    webfont

--- webfont/_js/b_draw.js
+++ webfont/_js/b_draw.js
@@ -12,11 +12,14 @@ function setup() {
 
 function draw() {
   background(75, 100, 100);
+  // background(255);
   noStroke();
   fill(20, 100, 100);
+  // fill(100);
   ellipse(mouseX, windowHeight/4, windowWidth/1.7, windowHeight/1.7);
   ellipse(mouseX, windowHeight*3/4, windowWidth/1.7, windowHeight/1.7);
   fill(90, 100, 100);
+  // fill(0);
   rect(0, 0, windowWidth/2, windowHeight*1.7);
 
 // if(mouseX<windowWidth/2) {
